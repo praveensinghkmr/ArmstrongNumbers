8ccfff201847a616203d99db307898792678f947
praveensinghkmr <praveensingh.kmr@gmail.com>
2018-10-01 17:13:32 +0530

    Implemented Logic

--- ArmstrongNumbers.cs
+++ ArmstrongNumbers.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int num = number;
+        int sum = 0;
+        List<int> digits = new List<int>();
+
+        do
+        {
+            digits.Add(num % 10);
+            num = num/10;
+        } while (num != 0);
+
+        foreach(int digit in digits)
+        {
+            sum = sum + (int)Math.Pow(digit, digits.Count);
+        }
+
+        if (sum == number)
+            return true;
+
+        return false;
     }
 }
